f87007ee268f49383658ccfd33d8af26a847a87e
Your Name <you@example.com>
2022-09-20 13:30:00 +0530

    added:logic for cash counter

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+        CashCounter counter = new CashCounter();
+        counter.enqueue(500);
+        counter.enqueue(1000);
+        counter.enqueue(200);
+        counter.enqueue(400);
+        counter.dequeue();
+        System.out.println(counter.noOfPersonInQueue());
+
+    }
+}
