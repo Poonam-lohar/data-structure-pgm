9b80aac6071f51509011db13c7b9b738d8810731
Your Name <you@example.com>
2022-09-20 14:37:08 +0530

    added:logic for prime number in 2D array

---
+++ src/com/bridgelabz/DMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class DMain {
+    public static void main(String[] args) {
+        PrimeTo2D primeNo = new PrimeTo2D();
+        primeNo.primeNo();
+        primeNo.show();
+
+    }
+}
